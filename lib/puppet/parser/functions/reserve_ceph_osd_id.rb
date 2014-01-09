bb7d646f696db4064677a2b45428ba66d94906ee
Atze de Vries <atze.devries@naturalis.nl>
2014-01-09 10:58:31 +0100

    testing osd.pp

--- lib/puppet/parser/functions/reserve_ceph_osd_id.rb
+++ lib/puppet/parser/functions/reserve_ceph_osd_id.rb
@@ -1,10 +1,11 @@
 
 module Puppet::Parser::Functions
       newfunction(:reserve_ceph_osd_id, :type => :rvalue) do |args|
-      	require 'open3'
-        stdin, stdout, stderr = Open3.popen3('cat',@options['/tmp/test.file'])
-        return stdout
+      	#require 'open3'
+        #stdin, stdout, stderr = Open3.popen3('cat',@options['/tmp/test.file'])
+        #return stdout
         #out = IO.popen(["cat","/tmp/test.file"]).read
         #return out
+        result = %x[ls]
       end
     end
