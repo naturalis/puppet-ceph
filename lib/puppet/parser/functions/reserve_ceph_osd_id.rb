93b7156bd6e74d33318178010cf0a9fe7eb36984
Atze de Vries <atze.devries@naturalis.nl>
2014-01-09 10:33:44 +0100

    testing osd.pp

--- lib/puppet/parser/functions/reserve_ceph_osd_id.rb
+++ lib/puppet/parser/functions/reserve_ceph_osd_id.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
       newfunction(:reserve_ceph_osd_id, :type => :rvalue) do |args|
-        val = `ls`
+        val = `ls / 2>&1`
         return '7'
       end
     end
