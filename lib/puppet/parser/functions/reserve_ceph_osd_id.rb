6c6a19d1182af68920b5ba9a4f70212dfaa275df
Atze de Vries <atze.devries@naturalis.nl>
2014-01-09 09:11:22 +0100

    testing osd.pp

--- lib/puppet/parser/functions/reserve_ceph_osd_id.rb
+++ lib/puppet/parser/functions/reserve_ceph_osd_id.rb
@@ -1,6 +1,6 @@
 module Puppet::Parser::Functions
       newfunction(:reserve_ceph_osd_id, :type => :rvalue) do |args|
         #system '/usr/bin/ceph osd create --keyring /tmp/monitor.keyring'
-        return '5'
+        return '5 #{args[0]}'
       end
     end
