ea25583eae1841553d5293b8992f98c00655a0d5
Atze de Vries <atze.devries@naturalis.nl>
2014-01-09 11:11:50 +0100

    testing osd.pp

--- lib/puppet/parser/functions/reserve_ceph_osd_id.rb
+++ lib/puppet/parser/functions/reserve_ceph_osd_id.rb
@@ -1,7 +1,6 @@
 
 module Puppet::Parser::Functions
       newfunction(:reserve_ceph_osd_id, :type => :rvalue) do |args|
-      	system 'hostname > /tmp/hn.txt'
       	#require 'open3'
         #stdin, stdout, stderr = Open3.popen3('cat',@options['/tmp/test.file'])
         #return stdout
