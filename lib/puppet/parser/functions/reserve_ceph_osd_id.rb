1a5d4c2158f2c38cafabf8c3fa29d22214a0c86a
Atze de Vries <atze.devries@naturalis.nl>
2014-01-09 10:02:25 +0100

    testing osd.pp

--- lib/puppet/parser/functions/reserve_ceph_osd_id.rb
+++ lib/puppet/parser/functions/reserve_ceph_osd_id.rb
@@ -2,7 +2,8 @@ module Puppet::Parser::Functions
       newfunction(:reserve_ceph_osd_id, :type => :rvalue) do |args|
       	#keyringfile = args[0]
         #system '/usr/bin/ceph osd create --keyring /tmp/monitor.keyring'
-        system 'cat /tmp/test.file'
+        #system 'cat /tmp/test.file'
+        system 'echo "4"'
         #return keyringfile
       end
     end
--- manifests/osd/generate.pp
+++ manifests/osd/generate.pp
@@ -4,6 +4,7 @@ define ceph::osd::generate (
 {
   
   $osd_id = reserve_ceph_osd_id("2")
+
   notify {"osdid-${osd_id}-${disk}":
     message => $osd_id,
     require => File["${fqdn}-osd-temp-keyring"]
