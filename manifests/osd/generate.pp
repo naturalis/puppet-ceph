2864e5ebff36937b207e9ac8792d473b1a3db2be
Atze de Vries <atze.devries@naturalis.nl>
2014-01-09 09:20:53 +0100

    testing osd.pp

--- manifests/osd/generate.pp
+++ manifests/osd/generate.pp
@@ -3,7 +3,7 @@ define ceph::osd::generate (
 )
 {
   
-  $osd_id = reserve_ceph_osd_id("2")
+  $osd_id = reserve_ceph_osd_id(2)
   notify {"osdnumber-${osd_id}-${disk}":
     message => $osd_id,
     require => File["${fqdn}-osd-temp-keyring"]
