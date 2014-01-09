4ca87883c3a7d7c284e2ccb3ec0887171a167f04
Atze de Vries <atze.devries@naturalis.nl>
2014-01-09 10:27:22 +0100

    testing osd.pp

--- lib/puppet/parser/functions/reserve_ceph_osd_id.rb
+++ lib/puppet/parser/functions/reserve_ceph_osd_id.rb
@@ -1,5 +1,6 @@
 module Puppet::Parser::Functions
       newfunction(:reserve_ceph_osd_id, :type => :rvalue) do |args|
-        return '6'
+        val = system 'cat /tmp/test.file'
+        return '7'
       end
     end
