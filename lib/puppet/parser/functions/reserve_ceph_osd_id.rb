6a3d23b790639e6c9525c0f7b0ba5970cbd383a9
Atze de Vries <atze.devries@naturalis.nl>
2014-01-09 10:39:55 +0100

    testing osd.pp

--- lib/puppet/parser/functions/reserve_ceph_osd_id.rb
+++ lib/puppet/parser/functions/reserve_ceph_osd_id.rb
@@ -1,6 +1,7 @@
+require 'open3'
 module Puppet::Parser::Functions
       newfunction(:reserve_ceph_osd_id, :type => :rvalue) do |args|
-        val = `ls / 2>&1`
-        return '7'
+        stdin, stdout, stderr = Open3.popen3('ls /')
+        return stdout
       end
     end
