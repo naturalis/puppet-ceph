5fb9619bfd1fef5dfdcfaaee82a8c4c931e5792a
Atze de Vries <atze.devries@naturalis.nl>
2014-01-11 11:22:46 +0100

    building ceph osd with facts

--- manifests/osd/prepare.pp
+++ manifests/osd/prepare.pp
@@ -5,7 +5,8 @@ define ceph::osd::prepare (
   
   exec { "${fqdn}-${disk}-generate-osd-id-fact":
     command => "/usr/bin/python /tmp/generate_ceph_osd_id_fact.py ${disk}",
-    require => File["${fqdn}-osd-generate-script"],
+    require => [File["${fqdn}-osd-generate-script"],
+                File["${fqdn}-osd-temp-keyring"]],
     unless  => "/bin/grep ${disk} /etc/facter/facts.d/ceph-osd-id-array.txt",
   }
 }
