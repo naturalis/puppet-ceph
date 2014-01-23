7d0cc211c3e69bc159baa93d0208296ac82b76b5
Atze de Vries <atze.devries@naturalis.nl>
2014-01-23 16:31:43 +0100

    start on radosgw

--- manifests/package/radosgw.pp
+++ manifests/package/radosgw.pp
@@ -28,7 +28,7 @@ deb http://gitbuilder.ceph.com/apache2-deb-precise-x86_64-basic/ref/master/ prec
     require => File['/etc/apt/sources.list.d/radosgw.list'],
   }
   
-  exec { '/usr/bin/apt-get install -y -q --force-yes apache2 libapache-mod-fastcgi':
+  exec { '/usr/bin/apt-get install -y -q --force-yes apache2 libapache2-mod-fastcgi':
     require => Exec['update-apt-get'],
   }
 
