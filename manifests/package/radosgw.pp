01cfad21cd20685ddeceb841e5566094f925e282
Atze de Vries <atze.devries@naturalis.nl>
2014-01-23 15:54:44 +0100

    start on radosgw

--- manifests/package/radosgw.pp
+++ manifests/package/radosgw.pp
@@ -9,10 +9,18 @@ class ceph::package::radosgw(
     ensure => installed,
   }
   
+  exec { 'add-ceph-repo-key':
+    command => "/usr/bin/wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | sudo apt-key add -",
+    require => Package ['wget'],
+    unless   => "/usr/bin/apt-key list | grep Ceph"
+  }
+  
+
   file{ '/etc/apt/sources.list.d/radosgw.list':
     ensure  => present,
     content => 'deb http://gitbuilder.ceph.com/libapache-mod-fastcgi-deb-precise-x86_64-basic/ref/master/ precise main
 deb http://gitbuilder.ceph.com/apache2-deb-precise-x86_64-basic/ref/master/ precise main',
+    require => Exec['add-ceph-repo-key'],
   }
 
   exec { 'update-apt-get':
@@ -24,5 +32,8 @@ deb http://gitbuilder.ceph.com/apache2-deb-precise-x86_64-basic/ref/master/ prec
     ensure => installed,
     require => Exec['update-apt-get'],
   }
+
+
+ 
   
 }
