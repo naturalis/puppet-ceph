36932a44b6cee6c3ec840cde21c0af3939937287
Atze de Vries <atze.devries@naturalis.nl>
2014-01-27 17:03:12 +0100

    added client package to radosgw

--- manifests/package/radosgw.pp
+++ manifests/package/radosgw.pp
@@ -9,18 +9,17 @@ class ceph::package::radosgw(
   #  ensure => installed,
   #}
   
-  exec { 'add-ceph-repo-key':
-    command => "/usr/bin/wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | sudo apt-key add -",
-    require => Package ['wget'],
-    unless   => "/usr/bin/apt-key list | grep Ceph"
-  }
+ # exec { 'add-ceph-repo-key':
+ #  command => "/usr/bin/wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | sudo apt-key add -",
+ #   require => Package ['wget'],
+ #   unless   => "/usr/bin/apt-key list | grep Ceph"
+ # }
   
 
   file{ '/etc/apt/sources.list.d/radosgw.list':
     ensure  => present,
     content => 'deb http://gitbuilder.ceph.com/libapache-mod-fastcgi-deb-precise-x86_64-basic/ref/master/ precise main
 deb http://gitbuilder.ceph.com/apache2-deb-precise-x86_64-basic/ref/master/ precise main',
-    require => Exec['add-ceph-repo-key'],
   }
 
   exec { 'update-apt-get':
@@ -35,18 +34,8 @@ deb http://gitbuilder.ceph.com/apache2-deb-precise-x86_64-basic/ref/master/ prec
   #exec { '/usr/bin/apt-get install -y -q --force-yes apache2':
   #  require => Exec['update-apt-get'],
   #}
-  exec {'get-new-lib-curl':
-    command => '/usr/bin/wget -q http://security.ubuntu.com/ubuntu/pool/main/c/curl/libcurl3_7.29.0-1ubuntu3.4_amd64.deb',
-    cwd     => '/tmp',
-    require => Package ['wget'],
-  }
-
-  exec {'install-new-lib-curl':
-    command => '/usr/bin/dpkg -i /tmp/libcurl3_7.29.0-1ubuntu3.4_amd64.deb',
-
-  }
-  
-  force-apt-install { ['apache2','libapache2-mod-fastcgi']:
+    
+ force-apt-install { ['apache2','libapache2-mod-fastcgi']:
     require => Exec['update-apt-get'],
   }
 
