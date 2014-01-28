9414cf32be7dd21e79d2be7a1a0fc09eb3a4182e
Atze de Vries <atze.devries@naturalis.nl>
2014-01-28 13:33:27 +0100

    created rados class

---
+++ manifests/radosgw.pp
@@ -0,0 +1,23 @@
+class ceph::radosgw (
+  rados_server_name 			= $fqdn,
+  rados_server_alias 			= $fqdn,
+  rados_server_webmaster_alias = 'aut@naturalis.nl',
+){
+  
+  
+  file {'/etc/apache2/sites-available/radosgw.vhost':
+   	ensure	=> present,
+  	content	=> template('ceph/radosgw.vhost.erb'),
+    owner	=> 'root',
+    group 	=> 'www-data',
+    mode	=> '0660',
+  }
+
+  file {'/var/www/radosgw.fcgi':
+  	ensure	=> present,
+  	content	=> template('ceph/radosgw.fcgi.erb'),
+  	owner	=> 'root',
+  	groups	=> 'www-data',
+  	mode	=> '0770',
+  }
+}
