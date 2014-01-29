859998ebad400af8a587b1c858aa8fae962a9522
Atze de Vries <atze.devries@naturalis.nl>
2014-01-29 13:41:22 +0100

    added apache mods, sites and services

--- manifests/radosgw.pp
+++ manifests/radosgw.pp
@@ -22,4 +22,48 @@ class ceph::radosgw (
     group    => 'www-data',
   	mode     => '0770',
   }
+
+  exec {'/usr/sbin/a2ensite radosgw.vhost':
+    unless  => '/bin/test -h /etc/apache2/sites-enabled/radosgw.vhost',
+    require => File['/etc/apache2/sites-available/radosgw.vhost'],
+    notice  => Service['apache2'],
+  }
+
+  exec {'/usr/sbin/a2dissite default':
+    onlyif  => '/bin/test -h /etc/apache2/sites-enabled/000-default',
+    notice  => Service['apache2'],
+  }
+ 
+  exec {'/usr/sbin/a2enmod rewrite':
+    unless => '/bin/test -h /etc/apache2/mods-enabled/rewrite.load',
+    notice => Service['apache2'],
+  }
+
+  exec {'/usr/sbin/a2enmod fastcgi':
+    unless => '/bin/test -h /etc/apache2/mods-enabled/fastcgi.load',
+    notice => Service['apache2']
+  }
+
+  service {'apache2': 
+    ensure => 'enabled'
+  }
+
+  file {['/var/lib/ceph','/var/lib/ceph/radosgw',"/var/lib/ceph/ceph-radosgw.${hostname}"]:
+    ensure   => directory,
+    owner    => 'root',
+    group    => 'root',
+    mode     => '0770',
+  }
+
+  file {"/var/lib/ceph/ceph-radosgw.${hostname}/done":
+    ensure   => present,
+    content  => '',
+    owner    => 'root',
+    group    => 'root',
+    mode     => '0660',
+    require  => File["/var/lib/ceph/ceph-radosgw.${hostname}"],
+  }
+
+  
+
 }
