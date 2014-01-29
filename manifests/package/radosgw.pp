ccc2e9935982d47dfdb60780f18a987d0766e2dd
Atze de Vries <atze.devries@naturalis.nl>
2014-01-29 14:02:20 +0100

    added radosgw package

--- manifests/package/radosgw.pp
+++ manifests/package/radosgw.pp
@@ -26,6 +26,10 @@ deb http://gitbuilder.ceph.com/apache2-deb-precise-x86_64-basic/ref/master/ prec
     command => '/usr/bin/apt-get update',
     require => File['/etc/apt/sources.list.d/radosgw.list'],
   }
+
+  package {'ragosgw':
+    ensure => installed,
+  }
   
   #exec { '/usr/bin/apt-get install -y -q --force-yes libapache2-mod-fastcgi':
   #  require => Exec['update-apt-get'],
